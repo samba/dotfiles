eb21ad9fc0da21c87032875835834e75c6272268
Sam Briesemeister <sam@analyticspros.com>
2011-02-16 11:22:18 -0800

    WIP: transition Bash and Vim to dotfiles cache

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -2,27 +2,20 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
-export MY_BASH=$HOME/.dotfiles/bash/
 export DOTFILES=${HOME}/.dotfiles/
-export PATH=${PATH}:${HOME}/.dotfiles/bin/
+export PATH=${PATH}:${DOTFILES}/bin/
 
-. $MY_BASH/lib.sh
+. $DOTFILES/bash/lib.sh
+
+# NOTE: we're rescanning *every time* right now until this stabilizes
+dotfiles -s >/dev/null
 
 
 setup_login_shell () {
-	SKIP_DEFAULT=0 DEFAULT=auto ENTRYPT=bashrc.sh
-
-	for i in $USER@$HOSTNAME $DEFAULT; do
-		if [ $i = $DEFAULT ] && [ $SKIP_DEFAULT -gt 0 ]; then
-			SKIP_DEFAULT=0
-			continue;
-		fi
-		export p=$(get_shell_config $ENTRYPT "$i")
-		[ -z "$p" ] && continue
-		[ ! -f "$p" ] && continue
-		. "$p"
-	done
-
+  dotfiles -q '(*.)\.sh$' | while read i; do
+    echo '#' ${DOTFILES}/$i >&2
+    . ${DOTFILES}/$i
+  done
 }
 
 # load the enhancements for all login shells
--- bash/logout.sh
+++ bash/logout.sh
@@ -1,22 +1,9 @@
 # ~/.bash_logout: executed by bash(1) when login shell exits.
 
-export MY_BASH=$HOME/.dotfiles/bash/
-. $MY_BASH/lib.sh
-
+. ${DOTFILES}/bash/lib.sh
 
 cleanup_login_shell () {
-	SKIP_DEFAULT=0 DEFAULT=auto
-
-	for i in $USER@$HOSTNAME $DEFAULT; do
-		if [ $i = $DEFAULT ] && [ $SKIP_DEFAULT -gt 0 ]; then
-			SKIP_DEFAULT=0
-			continue;
-		fi
-		export p=$(get_shell_config logout.sh $i)
-    [ -z "$p" ] && continue
-    [ ! -f "$p" ] && continue
-    . "$p"
-	done
+  : # do nothing
 }
 
 is_login_shell && cleanup_login_shell
