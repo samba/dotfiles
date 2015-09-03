51ffe814756a7c9679ccb9efd5e844eb867b7297
Sam Briesemeister <sam.briesemeister@gmail.com>
2015-09-03 13:21:31 -0700

    Load PYTHONSTARTUP only when STDOUT is a TTY

--- dotfiles/usercustomize.py
+++ dotfiles/usercustomize.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-import os
-__startup__ = os.environ.get('PYTHONSTARTUP', None) 
-if __startup__:
+import os, sys
+__startup__ = os.environ.get('PYTHONSTARTUP', None)
+
+if __startup__ and os.isatty(sys.stdout.fileno()):
     __startup__ = os.path.expanduser(__startup__)
     if os.path.isfile(__startup__):
         execfile(__startup__)
