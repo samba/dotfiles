9474f072ae60e9fd975c6f62f5f53a92f07ea8df
Sam Briesemeister <sam.briesemeister@gmail.com>
2015-09-03 01:35:50 -0700

    Update downloads for Mac

--- installation-scripts/downloads.sh
+++ installation-scripts/downloads.sh
@@ -11,7 +11,7 @@ download () {
 
 setup_git_osx_keychain () {
 
-  git credential-osxkeychain >/dev/null
+  git credential-osxkeychain get >/dev/null
   if [ $? -ne 1 -a $? -ne 0 ]; then
     curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
     chmod u+x git-credential-osxkeychain
@@ -22,20 +22,28 @@ setup_git_osx_keychain () {
 
 }
 
+queue_mac_downloads () {
+  # download https://git-osx-installer.googlecode.com/files/git-1.8.2.2-intel-universal-snow-leopard.dmg &
+  # download https://macvim.googlecode.com/files/MacVim-snapshot-66-Lion.tbz &
+  download https://iterm2.com/downloads/stable/iTerm2_v2_0.zip iTerm2.zip &
+  download https://central.github.com/mac/latest GithubForMac.zip &
+  download http://www.irradiatedsoftware.com/download/Cinch.zip Cinch.zip &
+  download https://kapeli.com/downloads/v3/Dash.zip Dash.zip &
+  download http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203083.dmg Sublime\ Text\ 3.dmg &
+  download https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg Google\ Chrome.dmg &
+  download https://update.cyberduck.io/Cyberduck-4.7.2.zip Cyberduck\ 4.7.1.zip &
+  setup_git_osx_keychain
+}
+
 
 queue_downloads () {
   DOWNLOAD_IN_PROGRESS=1
   echo "# Downloading utilities to ${DOWNLOADS} (You will need to install these manually)">&2
 
   mkdir -p ${DOWNLOADS}
-  download https://central.github.com/mac/latest GithubForMac.zip &
-  download https://git-osx-installer.googlecode.com/files/git-1.8.2.2-intel-universal-snow-leopard.dmg &
-  download https://macvim.googlecode.com/files/MacVim-snapshot-66-Lion.tbz &
-  download https://iterm2.googlecode.com/files/iTerm2-1_0_0_20130319.zip iTerm2.zip &
-  download http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg Sublime\ Text\ 2.dmg &
-
-  setup_git_osx_keychain
-
+ 
+  uname | grep Darwin && queue_mac_downloads; 
+  
   # http://majutsushi.github.io/tagbar/
   # download http://github.com/majutsushi/tagbar/tarball/v2.5 vim-tagbar-2.5.tar.gz &
 }
