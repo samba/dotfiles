576889a6bfd5e268367a36ef0cc8364779d26b83
Sam Briesemeister <sam.briesemeister@gmail.com>
2015-09-07 22:02:13 -0700

    More conditional handling to mitigate errors with AppEngine loading Python

--- dotfiles/usercustomize.py
+++ dotfiles/usercustomize.py
@@ -6,22 +6,24 @@
 
 import os
 import sys
-import __main__ as main
+
+try:
+    import __main__ as main
+except ImportError:
+    main = None
 
 class _UserCustom(object):
     __startup__ = os.environ.get('PYTHONSTARTUP', None)
 
-
-
     @classmethod
     def interactive(cls):
-        filepath = getattr(main, '__file__', None)
+        filepath = main and getattr(main, '__file__', None)
 
         # Somehow it was being loaded "interactively" by AppEngine and other SDKs
         # and throwing errors, so now we'll validate that it's loading via terminal.
         terminal = os.isatty(sys.stdin.fileno()) and os.isatty(sys.stdout.fileno())
 
-        if terminal and sys.flags.interactive: # invoked with -i
+        if terminal and (sys.flags.interactive > 0): # invoked with -i
             return 1 if (not filepath) else 2
 
         # NOTE: other cases here removed;
@@ -34,15 +36,18 @@ class _UserCustom(object):
 
     @classmethod
     def valid_startup(cls):
-        result = os.path.expanduser(cls.__startup__)
+        result = cls.__startup__ and os.path.expanduser(cls.__startup__)
         return result if (os.path.isfile(result)) else None
 
     @classmethod
     def _import(cls):
-        _path = cls.valid_startup()
         _interactive = cls.interactive()
-        # print >>sys.stderr, "Interactive mode %r" % _interactive
-        if _path and (_interactive in (1,2)):
+        _path = (_interactive in (1, 2)) and cls.valid_startup()
+
+        # print >>sys.stderr, "<%s> Interactive mode %r" % (__file__, _interactive)
+        # print "<%s> Interactive mode %r" % (__file__, _interactive)
+        
+        if _path:
             execfile(_path, { 'RUN_INTERACTIVE': _interactive })
 
 
