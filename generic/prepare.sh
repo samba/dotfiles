1da854e886cf284283abc02f9747a351f0ec2b7a
Sam Briesemeister <sam.briesemeister@gmail.com>
2018-05-25 13:59:44 -0700

    Git config stash generates proper restore script

--- generic/prepare.sh
+++ generic/prepare.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
 
+# Generate a static script that restores the proper git config.
+# This will make recovery easier in the event that a task somewhere else in this process fails.
 populate_params_stash () {
 cat >> ${2} <<EOF
+#!/usr/bin/env sh
 # Stashed properties from ~/.gitconfig
-GIT_USER_NAME="`git config -f $1 --get user.name`"
-GIT_USER_EMAIL="`git config -f $1 --get user.email`"
-GITHUB_USER="`git config -f $1 --get github.user`"
-GITHUB_EMAIL="`git config -f $1  --get github.email`"
-GITHUB_TOKEN="`git config -f $1  --get github.token`"
+GIT_USER_NAME="$(git config -f $1 --get user.name)"
+GIT_USER_EMAIL="$(git config -f $1 --get user.email)"
+GITHUB_USER="$(git config -f $1 --get github.user)"
+GITHUB_EMAIL="$(git config -f $1  --get github.email)"
+GITHUB_TOKEN="$(git config -f $1  --get github.token)"
+
+
+if test -f \$1; then
+  git config -f \$1 user.name "\${GIT_USER_NAME}"
+  git config -f \$1 user.email "\${GIT_USER_EMAIL}"
+  git config -f \$1 github.user "\${GITHUB_USER}"
+  git config -f \$1 github.email "\${GITHUB_EMAIL}"
+  git config -f \$1 github.token "\${GITHUB_TOKEN}"
+else 
+  echo "Could not find Git config file \$1" >&2
+fi
 EOF
 }
 
-
 restore_params_config () {
-    test -f $2 && . $2 || return $?
-    [ -z "${GIT_USER_NAME}" ] || git config -f $1  user.name "${GIT_USER_NAME}"
-    [ -z "${GIT_USER_EMAIL}" ] || git config -f $1 user.email "${GIT_USER_EMAIL}"
-    [ -z "${GITHUB_USER}" ] || git config -f $1 github.user "${GITHUB_USER}"
-    [ -z "${GITHUB_EMAIL}" ] || git config -f $1  github.email "${GITHUB_EMAIL}"
-    [ -z "${GITHUB_TOKEN}" ] || git config -f $1  github.token "${GITHUB_TOKEN}"
+    if test -f $2; then
+      echo "Restoring Git configuration $2 > $1" >&2
+      bash -x $2 $1
+    else
+      echo "Could not find gitconfig restore script: $2" >&2
+      return 1
+    fi
 }
 
 setup_ssh_keys () {
