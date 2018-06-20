3aad1d2200d1e29fddd0ba41ecd9afa8b866ce32
Sam Briesemeister <sam.briesemeister@gmail.com>
2018-06-20 07:43:44 -0700

    SSH key handling automation is now ... silent.

--- dotfiles/.bash_include.d/sshkeys.sh
+++ dotfiles/.bash_include.d/sshkeys.sh
@@ -2,6 +2,7 @@
 
 # Loads SSH keys in a sensible fashion
 
+
 ssh_agent_avail () {
 		test -z "$SSH_AGENT_PID" && return 1
 		test -r "$SSH_AUTH_SOCK" || return 1 
@@ -10,24 +11,24 @@ ssh_agent_avail () {
 }
 
 configure_ssh_agent () {
-
-	ssh-add -q -A  # automatically add keychain-provided keys
+	# automatically add keychain-provided keys
+	ssh-add -q -A 1>/dev/null 2>/dev/null
 
 	# Discover keys that are not already known
 	find "${HOME}/.ssh" -type f -name '*rsa' \
-		| grep -v -f <(ssh-add -l | cut -d ' ' -f 3) \
-		| xargs ssh-add -q -K
+		| grep -v -f <(ssh-add -l 2>/dev/null | cut -d ' ' -f 3) \
+		| xargs ssh-add -q -K 1>/dev/null   2>/dev/null
 
 
 	# List known keys
-	ssh-add -l
+	# ssh-add -l
 }
 
 if shopt -q login_shell; then
-	test -z "$SSH_AGENT_PID" && eval "$(ssh-agent -s)"
+	which ssh-agent >/dev/null && \
+		test -z "$SSH_AGENT_PID" && \
+		eval "$(ssh-agent -s 2>/dev/null)" 1>/dev/null
 	ssh_agent_avail && configure_ssh_agent
 	echo
 fi
-
-
 
