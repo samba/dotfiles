62d47a9a1d0a2d84f3e12bf95db70982c689ca11
Sam Briesemeister <sam.briesemeister@gmail.com>
2018-10-05 12:14:31 -0700

    vim: install modules via git (bypass ping)
    Docker somehow interferes with pinging common internet hosts (e.g. google.com)
    The script flags allows it to fail properly on the first `git clone` failure.

--- util/vimsetup.sh
+++ util/vimsetup.sh
@@ -21,15 +21,18 @@ function gitclone () {
 
 }
 
+function vim:install_packaages () {
+    gitclone "${HOME}/.vim/modules/pathogen" "https://github.com/tpope/vim-pathogen.git"
+    gitclone "${HOME}/.vim/bundle/vim-json" "https://github.com/elzr/vim-json.git"
+    gitclone "${HOME}/.vim/bundle/vim-openssl" "https://github.com/vim-scripts/openssl.vim.git"
+    gitclone "${HOME}/.vim/bundle/vim-markdown" "https://github.com/tpope/vim-markdown.git"
+}
+
 function main () {
-    mkdir -p ${HOME}/.vim/{backup,swap,modules,bundle,doc,autoload,syntax,plugin,ftdetect}
-	ping -q -c 1 -W 1 google.com && {
-		gitclone "${HOME}/.vim/modules/pathogen" "https://github.com/tpope/vim-pathogen.git" && \
-		gitclone "${HOME}/.vim/bundle/vim-json" "https://github.com/elzr/vim-json.git" && \
-		gitclone "${HOME}/.vim/bundle/vim-openssl" "https://github.com/vim-scripts/openssl.vim.git" && \
-		gitclone "${HOME}/.vim/bundle/vim-markdown" "https://github.com/tpope/vim-markdown.git" && \
-		cd "${HOME}/.vim/autoload" && ln -sf ../modules/pathogen/autoload/pathogen.vim ./pathogen.vim; \
-	}
+    mkdir -p ${HOME}/.vim/{backup,swap,modules,bundle,doc,autoload,syntax,plugin,ftdetect};
+	ping -q -c 1 -W 1 google.com || echo -e "\n\n###_____ COULD NOT VERIFY INTERNET ACCESS  ____<<<<\n\n" >&2
+    vim:install_packaages || return $?;
+	cd "${HOME}/.vim/autoload" && ln -sf ../modules/pathogen/autoload/pathogen.vim ./pathogen.vim;
 }
 
 main "$@"
